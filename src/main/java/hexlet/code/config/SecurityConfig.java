fb7402bc96adcb7f9cbede2dfea1fe6a55644666
maslievV <maslievvlad8@gmail.com>
2025-05-29 16:21:02 +0300

    connect frontend, fix login

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -40,6 +40,10 @@ public class SecurityConfig {
                         // Разрешаем доступ только к /api/login, чтобы аутентифицироваться и получить токен
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/welcome").permitAll()
+                        .requestMatchers("/test").permitAll()
+                        .requestMatchers("/index.html").permitAll()
+                        .requestMatchers("/assets/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .oauth2ResourceServer((rs) -> rs.jwt((jwt) -> jwt.decoder(jwtDecoder)))
--- src/main/java/hexlet/code/controller/api/AuthenticationController.java
+++ src/main/java/hexlet/code/controller/api/AuthenticationController.java
@@ -23,11 +23,11 @@ public class AuthenticationController {
     @PostMapping
     public String create(@RequestBody AuthRequest authRequest) {
         var authentication = new UsernamePasswordAuthenticationToken(
-                authRequest.getEmail(), authRequest.getPassword());
+                authRequest.getUsername(), authRequest.getPassword());
 
         authenticationManager.authenticate(authentication);
 
-        var token = jwtUtils.generateToken(authRequest.getEmail());
+        var token = jwtUtils.generateToken(authRequest.getUsername());
         return token;
     }
 
